ea5b121a9ad0d20c84edd17d51c5e8a370c55da6
AisyatulHuriyah <aisyatulhuriyah08@gmail.com>
2025-10-02 14:30:36 +0700

    added user 2

--- Test.java
+++ Test.java
@@ -1,6 +1,6 @@
 public class Test{
     public static void main(String[] args) {
-        System.out.println("Hi, this is first line code modified by user 1");
-        System.out.println("User 1 added new line");
+        System.out.println("Hi, this is first line code modified by user 2");
+        System.out.println("User 2 added new line");
     }
 }
