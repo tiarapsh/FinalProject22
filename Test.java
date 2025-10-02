b2ef0a05b05d46ea72c3f2aaf6d4683fc94a9923
tiara <haiiiinirara@gmail.com>
2025-10-02 14:34:28 +0700

    try added

--- Test.java
+++ Test.java
@@ -2,5 +2,6 @@ public class Test{
     public static void main(String[] args) {
         System.out.println("Hi, this is first line code modified by user 2");
         System.out.println("User 2 added new line");
+        System.out.println("This is new line added by user 2");
     }
 }
